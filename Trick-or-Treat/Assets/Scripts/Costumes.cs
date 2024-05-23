86949540f3223e27829cf5dd70e9b70ea3c72f52
Peter Ochoa <112195599+Peter8a473@users.noreply.github.com>
2024-05-23 01:45:21 -0400

    Costume Menu pt1

--- Trick-or-Treat/Assets/Scripts/Costumes.cs
+++ Trick-or-Treat/Assets/Scripts/Costumes.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Costumes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Sprite[] costumeIMG = new Sprite[16];
+    public static bool[] selection = new bool[16];
+
+    int selectionCount;
+    public TextMeshProUGUI selectionText;
+
+    public Toggle[] buttons = new Toggle[16];
+
+    void Awake()
     {
-        
+        for (int i = 0; i < 16; i++)
+        {
+            selection[i] = false;
+            costumeIMG[i] = buttons[i].gameObject.GetComponentInChildren<Image>().sprite;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    void OnEnable()
     {
-        
+        selectionCount = Customize.costumes;
+        for (int i = 0; i < 16; i++)
+            if (selection[i]) { selectionCount--; }
+
+        selectionText.text = selectionCount.ToString();
+    }
+
+    public void ToggleSprite(int index)
+    {
+        if (selection[index])
+        {
+            selection[index] = false;
+            if (selectionCount++ == 0)
+                EnableButtons(true);
+        }
+        else
+        {
+            selection[index] = true;
+            if (--selectionCount == 0)
+                EnableButtons(false);
+        }
+
+        selectionText.text = selectionCount.ToString();
+    }
+
+    void EnableButtons(bool state)
+    {
+        for (int i = 0; i < 16; i++)
+        {
+            if (!selection[i])
+                buttons[i].interactable = state;
+        }
     }
 }
--- Trick-or-Treat/Assets/Scripts/SliderPopup.cs
+++ Trick-or-Treat/Assets/Scripts/SliderPopup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,49 @@ using UnityEngine.EventSystems;
 public class SliderPopup : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private GameObject Popup;
-    bool visible;
+    public float countdown = 0;
+    public bool visible;
+    public bool pointer;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Popup.SetActive(true);
+        pointer = true;
+        TriggerPopup();
     }
 
     public void OnPointerExit(PointerEventData eventData) 
     {  
-        Popup.SetActive(false);
+        pointer = false;
     }
+
+    public void Slide()
+    {
+        countdown = 0.75f;
+    }
+
+    public void TriggerPopup()
+    {
+        if (!visible && gameObject.activeInHierarchy)
+        {
+            StartCoroutine(DisplayPopup());
+        }
+    }
+
+    public IEnumerator DisplayPopup()
+    {
+        visible = true;
+        Popup.SetActive(true);
+        while (countdown >= 0)
+        {
+            countdown -= Time.deltaTime;
+            if (pointer && countdown < 0)
+                countdown = 0;
+            
+            yield return null;
+        }
+        Popup.SetActive(false);
+        visible = false;
+        countdown = 0;
+    }
+
 }
